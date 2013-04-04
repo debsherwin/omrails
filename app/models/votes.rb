a62b4aa9049ce16d2e7acaeeed1a0b2bf1330a0b
Deb Sherwin <deborahsherwin@gmail.com>
2013-04-03 21:17:17 -0500

    fix

--- app/models/votes.rb
+++
@@ -1,3 +0,0 @@
-class Votes < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Omrails::Application.routes.draw do
   resources :pins
 
-
   devise_for :users
 
   get 'about' => 'pages#about'
